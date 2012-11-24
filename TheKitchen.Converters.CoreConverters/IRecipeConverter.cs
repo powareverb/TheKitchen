49a27e76a5792e649d98bec55515c1d44d71d806
Gavin Jones <gavin@phoenixdesign.co.nz>
2012-11-24 16:39:59 +1300

    WIP

---
+++ TheKitchen.Converters.CoreConverters/IRecipeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheKitchen.Model.Models;
+
+namespace TheKitchen.Converters.CoreConverters
+{
+    public interface IRecipeConverter
+    {
+        Recipe ToRecipe();
+
+        void Load(string recipeML);
+    }
+}
---
+++ TheKitchen.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheKitchen.Converters.CoreConverters;
+using TheKitchen.Model.Models;
+
+namespace TheKitchen.UnitOfMeasurements.Tests
+{
+    [TestClass]
+    public class RecipeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string recipeML = TestResources.RecipeML1;
+            IRecipeConverter recipeConverter = new RecipeMLConverter();
+            recipeConverter.Load(recipeML);
+            Recipe r = recipeConverter.ToRecipe();
+        }
+    }
+}
